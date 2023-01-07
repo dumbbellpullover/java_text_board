ff41123e0ba021fa2e64b8ee6ddd01487c497d1b
dumbbellpullover <dumbbellpullover@gmail.com>
2023-01-07 18:17:39 +0900

    ãoverride the method toString and debug classes

--- src/com/ukj/exam/board/Main.java
+++ src/com/ukj/exam/board/Main.java
@@ -1,5 +1,4 @@
 package com.ukj.exam.board;
-
 import java.util.Scanner;
 
 public class Main {
@@ -10,10 +9,11 @@ public class Main {
     System.out.println("== 게시판 v 0.1 ==");
     System.out.println("== 프로그램 시작 ==");
 
+    int articleLastId = 0;
+
     while (true) {
       System.out.print("명령 > ");
       String cmd = sc.nextLine();
-
 
       if (cmd.equals("exit")) {
         break;
@@ -24,8 +24,12 @@ public class Main {
         String title = sc.nextLine();
         System.out.printf("내용: ");
         String body = sc.nextLine();
-        int id = 1;
-        System.out.printf("%d번 게시물이 등록되었습니다.\n", id);
+        int id = ++articleLastId;
+
+        Article article = new Article(id, title, body);
+
+        System.out.println("생성된 게시물 객체: " + article);
+        System.out.printf("%d번 게시물이 등록되었습니다.\n", article.id);
       }
       else {
         System.out.printf("입력 된 명령어: %s\n", cmd);
@@ -34,5 +38,22 @@ public class Main {
 
     System.out.println("== 프로그램 종료 ==");
     sc.close();
+  }
+}
+
+class Article {
+  int id;
+  String title;
+  String body;
+
+  @Override
+  public String toString() {
+    return String.format("{id: %d, title: \"%s\", body: \"%s\"}", this.id, this.title, this.body);
+  }
+
+  Article(int id, String title, String body) {
+    this.id = id;
+    this.title = title;
+    this.body = body;
   }
 }
