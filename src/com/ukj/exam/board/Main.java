0765cf0fb10045825fd6abae57161759ff3eedd4
dumbbellpullover <dumbbellpullover@gmail.com>
2023-01-07 17:36:31 +0900

    input a new article

--- src/com/ukj/exam/board/Main.java
+++ src/com/ukj/exam/board/Main.java
@@ -14,8 +14,21 @@ public class Main {
       System.out.print("명령 > ");
       String cmd = sc.nextLine();
 
+
       if (cmd.equals("exit")) {
         break;
+      }
+      else if (cmd.equals("/usr/article/write")) {
+        System.out.println("== 게시물 등록 ==");
+        System.out.printf("제목: ");
+        String title = sc.nextLine();
+        System.out.printf("내용: ");
+        String body = sc.nextLine();
+        int id = 1;
+        System.out.printf("%d번 게시물이 등록되었습니다.\n", id);
+      }
+      else {
+        System.out.printf("입력 된 명령어: %s\n", cmd);
       }
     }
 
