93428ec3b966cf19d9d60544a626090046c2991e
dumbbellpullover <dumbbellpullover@gmail.com>
2023-01-07 22:23:57 +0900

    put new article in articles

--- src/com/ukj/exam/board/Main.java
+++ src/com/ukj/exam/board/Main.java
@@ -59,9 +59,9 @@ public class Main {
 
       } else if (cmd.equals("/usr/article/write")) {
         System.out.println("== 게시물 등록 ==");
-        System.out.printf("제목: ");
+        System.out.print("제목: ");
         String title = sc.nextLine();
-        System.out.printf("내용: ");
+        System.out.print("내용: ");
         String body = sc.nextLine();
         int id = ++articleLastId;
 
