d9dedd77ff731ebd2cd07d764db2253bcc780bc0
dumbbellpullover <dumbbellpullover@gmail.com>
2023-01-07 21:43:15 +0900

    print out a last article

--- src/com/ukj/exam/board/Main.java
+++ src/com/ukj/exam/board/Main.java
@@ -1,15 +1,16 @@
 package com.ukj.exam.board;
+
 import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) {
 
     Scanner sc = new Scanner(System.in);
+    int articleLastId = 0;
+    Article lastArticle = null;
 
     System.out.println("== 게시판 v 0.1 ==");
     System.out.println("== 프로그램 시작 ==");
-
-    int articleLastId = 0;
 
     while (true) {
       System.out.print("명령 > ");
@@ -17,8 +18,21 @@ public class Main {
 
       if (cmd.equals("exit")) {
         break;
-      }
-      else if (cmd.equals("/usr/article/write")) {
+
+      } else if (cmd.equals("/usr/article/detail")) {
+        Article article = lastArticle;
+
+        if (lastArticle == null) {
+          System.out.println("게시물이 존재하지 않습니다.");
+          continue;
+        }
+
+        System.out.println("== 게시물 상세 보기 ==");
+        System.out.printf("번호: %d\n", article.id);
+        System.out.printf("제목: %s\n", article.title);
+        System.out.printf("내용: %s\n", article.body);
+
+      } else if (cmd.equals("/usr/article/write")) {
         System.out.println("== 게시물 등록 ==");
         System.out.printf("제목: ");
         String title = sc.nextLine();
@@ -27,11 +41,12 @@ public class Main {
         int id = ++articleLastId;
 
         Article article = new Article(id, title, body);
+        lastArticle = article;
 
         System.out.println("생성된 게시물 객체: " + article);
         System.out.printf("%d번 게시물이 등록되었습니다.\n", article.id);
-      }
-      else {
+
+      } else {
         System.out.printf("입력 된 명령어: %s\n", cmd);
       }
     }
@@ -46,14 +61,14 @@ class Article {
   String title;
   String body;
 
-  @Override
-  public String toString() {
-    return String.format("{id: %d, title: \"%s\", body: \"%s\"}", this.id, this.title, this.body);
-  }
-
   Article(int id, String title, String body) {
     this.id = id;
     this.title = title;
     this.body = body;
+  }
+
+  @Override
+  public String toString() {
+    return String.format("{id: %d, title: \"%s\", body: \"%s\"}", this.id, this.title, this.body);
   }
 }
