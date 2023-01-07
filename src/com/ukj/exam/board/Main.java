529b35b1fe6721817ba8b7efa67017a9ae57715b
dumbbellpullover <dumbbellpullover@gmail.com>
2023-01-07 17:27:05 +0900

    input cmd while run

--- src/com/ukj/exam/board/Main.java
+++ src/com/ukj/exam/board/Main.java
@@ -9,13 +9,17 @@ public class Main {
 
     System.out.println("== 게시판 v 0.1 ==");
     System.out.println("== 프로그램 시작 ==");
-    System.out.print("명령 > ");
-    String cmd = sc.nextLine();
 
-    System.out.printf("입력된 명령어: %s\n", cmd);
+    while (true) {
+      System.out.print("명령 > ");
+      String cmd = sc.nextLine();
+
+      if (cmd.equals("exit")) {
+        break;
+      }
+    }
 
     System.out.println("== 프로그램 종료 ==");
-
     sc.close();
   }
 }
