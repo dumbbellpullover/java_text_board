af61a80172a917d68444284898e29a5924309a51
dumbbellpullover <dumbbellpullover@gmail.com>
2023-01-08 15:35:10 +0900

    make /usr/article/write function

--- src/com/ukj/exam/board/Main.java
+++ src/com/ukj/exam/board/Main.java
@@ -41,18 +41,9 @@ public class Main {
         actionUsrArticleDetail(rq, articles);
 
       } else if (rq.getUrlPath().equals("/usr/article/write")) {
-        System.out.println("== 게시물 등록 ==");
-        System.out.print("제목: ");
-        String title = sc.nextLine();
-        System.out.print("내용: ");
-        String body = sc.nextLine();
-        int id = ++articleLastId;
-
-        Article article = new Article(id, title, body);
-        articles.add(article);
-
-        System.out.println("생성된 게시물 객체: " + article);
-        System.out.printf("%d번 게시물이 등록되었습니다.\n", article.id);
+//        make /usr/article/write fucntion
+        actionUsrArticleWrite(sc, articles, articleLastId);
+        articleLastId++;
 
       } else {
         System.out.printf("입력 된 명령어: %s\n", cmd);
@@ -61,6 +52,21 @@ public class Main {
 
     System.out.println("== 프로그램 종료 ==");
     sc.close();
+  }
+
+  private static void actionUsrArticleWrite(Scanner sc, List<Article> articles, int articleLastId) {
+    System.out.println("== 게시물 등록 ==");
+    System.out.print("제목: ");
+    String title = sc.nextLine();
+    System.out.print("내용: ");
+    String body = sc.nextLine();
+    int id = ++articleLastId;
+
+    Article article = new Article(id, title, body);
+    articles.add(article);
+
+    System.out.println("생성된 게시물 객체: " + article);
+    System.out.printf("%d번 게시물이 등록되었습니다.\n", article.id);
   }
 
   private static void actionUsrArticleDetail(Rq rq, List<Article> articles) {
