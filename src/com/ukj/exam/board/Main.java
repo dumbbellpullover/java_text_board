50e6e2b5ba0bd80b7248defc5b6ecabc0ecf6b4a
dumbbellpullover <dumbbellpullover@gmail.com>
2023-01-08 13:53:14 +0900

    ãinput article num in url

--- src/com/ukj/exam/board/Main.java
+++ src/com/ukj/exam/board/Main.java
@@ -45,12 +45,14 @@ public class Main {
 
       } else if (rq.getUrlPath().equals("/usr/article/detail")) {
 
-        if (articles.isEmpty()) {
+        int id = Integer.parseInt(rq.getQueryParams().get("id"));
+
+        if (id > articles.size()) {
           System.out.println("게시물이 존재하지 않습니다.");
           continue;
         }
 
-        Article article = articles.get(articles.size() - 1);
+        Article article = articles.get(id - 1);
 
         System.out.println("== 게시물 상세 보기 ==");
         System.out.printf("번호: %d\n", article.id);
